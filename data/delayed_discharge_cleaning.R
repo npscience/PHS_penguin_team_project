987d58cf968503fb2710fe397eb62b4ebe4f526b
the-deadly-c0w <masdofswords@gmail.com>
2023-07-12 16:03:07 +0100

    cleaner clean

--- data/delayed_discharge_cleaning.R
+++ data/delayed_discharge_cleaning.R
@@ -8,17 +8,9 @@ delayed <- delayed %>%
          average_daily_number_of_delayed_beds,
          reason_for_delay)
 
-delayed_no_reason <- delayed %>% 
-  filter(reason_for_delay == "All Delay Reasons") %>% 
-  select(-reason_for_delay)
-
 delayed <- delayed %>% 
   mutate(month_of_delay = ym(month_of_delay))
 
-
-delayed_no_reason <- delayed_no_reason %>% 
-  mutate(month_of_delay = ym(month_of_delay))
-
 write_csv(delayed, "cleaned_data/delayed.csv")
-write_csv(delayed_no_reason, "cleaned_data/delayed_no_reason.csv")
+
 
