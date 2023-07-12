f779782ab8f1f35031b409f8930f89f652eb37b4
Naomi Penfold <9406530+npscience@users.noreply.github.com>
2023-07-12 15:45:41 +0100

    Revert "Merge branch 'feature_covid_ts_lines'"
    This reverts commit 2c87b9e3015104790a150f2a1e1eedca493741da, reversing
    changes made to 97e8435d4b8b886d198e452fb6e2988f65f9ce81.

---
+++ scripts/global.R
@@ -0,0 +1,54 @@
+# global -----
+
+library(shiny)
+
+library(tidyverse)
+library(janitor)
+library(lubridate)
+
+library(bslib)
+library(plotly)
+library(leaflet) # for maps
+library(sf) # if using geometry
+
+# data wrangling ----
+
+## load in data
+# for maps in general
+# hospitals <- read_csv("../data/cleaned_data/hospital_locations_clean.csv") 
+
+# for occupancy map and plot
+hospital_location_occupancy <- read_csv("../data/cleaned_data/hospital_location_occupancy.csv")
+occupancy_per_hb <- read_csv("../data/cleaned_data/occupancy_per_hb.csv") %>% 
+  mutate(quarter = zoo::as.yearqtr(quarter))
+
+### colour palette for hospitals on map
+pal <- colorFactor(c("navy", "blue", "steelblue", "skyblue",
+                     "red","indianred", "maroon", "brown",
+                     "springgreen", "springgreen2", "springgreen3", "springgreen4",
+                     "gold", "goldenrod", "yellow", "orange"),
+                   domain = unique(hospital_location_occupancy$hb))
+
+# lists for selectors ----
+
+
+hbs_list <- c("Ayrshire and Arran" = "S08000015",
+              "Borders" = "S08000016",
+              "Dumfries and Galloway" = "S08000017",
+              "Forth Valley" = "S08000019",
+              "Grampian" = "S08000020",
+              "Highland" = "S08000022",
+              "Lothian" = "S08000024",
+              "Orkney" = "S08000025",
+              "Shetland" = "S08000026",
+              "Western Isles" = "S08000028",
+              "Fife" = "S08000029",
+              "Tayside" = "S08000030",
+              "Greater Glasgow and Clyde" = "S08000031",
+              "Lanarkshire" = "S08000032",
+              "All Scotland" = "S92000003",
+              "The Golden Jubilee National Hospital" = "SB0801") # used on both pages for now
+
+
+covid_kpi_list <- c("Hospital admissions", "Percantage bed occupancy", "Delayed discharges")
+
