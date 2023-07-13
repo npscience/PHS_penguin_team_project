0f994f8b7f0eba51566be493f06a4a4e7a385069
ChiaraCapresi <chiara.capresi@yahoo.it>
2023-07-13 10:17:24 +0200

    update

--- team_notes/Chiara_notes/ha_admission_covid.R
+++ team_notes/Chiara_notes/ha_admission_covid.R
@@ -3,15 +3,51 @@ library(tidyverse)
 library(bslib)
 
 
-ha_demo <- read_csv("../PHS_penguin_team_project/team_notes/Chiara_notes/da_demo_clean.csv")
+ha_demo <- read_csv("../PHS_penguin_team_project/data/cleaned_data/ha_demo_clean.csv")
 
 
 
 ui <- fluidPage(
   
+  titlePanel(tags$h1("Covid impact on hospital admissions")),
+  
+  
+  plotOutput("ha_admissions"),
+  
+  
+  
+  
 )
 
+
+
+
+
+
+
+
+
+
+
 server <- function(input, output, session) {
+  
+  
+  output$ha_admissions <- renderPlot({
+    ha_demo %>% 
+      filter(hb %in% c("S08000015", "Scotland")) %>% 
+      group_by(hb, month_ending_date) %>% 
+      summarise(mean_admissions = mean(number_admissions)) %>% 
+      ggplot() +
+      aes(x = month_ending_date, y = mean_admissions, group = hb, colour = hb) +
+      geom_line()
+  })
+  
+  
+  
+  
+  
+  
+  
   
 }
 
